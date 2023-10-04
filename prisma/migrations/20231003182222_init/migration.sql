d8fa80a9d5a8062a029b44b77db65af1eaf36925
Mimachh <karl.mullr@gmail.com>
2023-10-04 23:41:03 +0200

    migrate en plus

--- prisma/migrations/20231003182222_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `terms` to the `Reservation` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `reservation` ADD COLUMN `terms` BOOLEAN NOT NULL;
--- prisma/migrations/20231003222339_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `date` on table `reservation` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `reservation` MODIFY `date` DATETIME(3) NOT NULL;
